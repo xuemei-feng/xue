4ea71dfe1816fc28c3da0947b7651d877e5876e6
root <root@node0.c220g2-518.stripe1219-pg0.wisc.cloudlab.us>
2026-05-29 01:33:05 -0500

    1423

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # 批量 RackCU 更新请求文件（改这一行即s可；相对 test.sh 所在目录或绝对路径）
-UPDATE_BATCH_FILE="/users/xue/xue/T00-result10000.txt"
+UPDATE_BATCH_FILE="/users/xue/xue/stripe Ten/T-64KB-10/T02-result10000.txt"
 
 SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
 cd "$SCRIPT_DIR" || exit 1
